229ad1b8eeff87fd664ee043355d6006e29884e2
Derek Danielson <derekdanielson84@gmail.com>
2020-11-03 09:50:07 -0700

    first commit

---
+++ freeCodeCampNotes.js
@@ -0,0 +1,70 @@
+//******JavaScript Comments******
+//Single line comment
+/*Multi-line
+comment*/
+
+
+//Declare JS Variables  ------  Use 'let' vs 'var'
+var exampleName;
+let exampleName;
+
+
+//Storing Values with the Assignment Operator
+let abc;
+abc = 38;    //abc will store 38
+
+
+//Assigning the Value of One Variable to Another
+let cat = 10;
+let dog = cat; // dog = 10
+
+
+//Initializing Variables with the Assignment Operator
+let car = 'Mustang';
+
+
+//Understanding Uninitialized Variables - When Variables are declared they have an initial value of 'undefined'.
+var ten = 10;
+var five = 5;
+var sentence = 'I am a ';
+
+ten = ten + 5;  // 15
+five = five + 1;  // 6
+sentence = sentence + 'string';  //'I am a string'
+
+
+//Add two numbers with JS
+let sum = 10 + 20; //30
+
+
+//Subtract One Number from Another  with JS
+let difference = 30 - 20; //10
+
+
+//Multiply 
+let multi = 10 * 8; //80
+
+
+//Divide
+let divide = 10 / 2; //5
+
+
+//Increment a number with JS  - Using ++ to add 1 number
+let myVar = 68;
+myVar++; //69
+
+
+//Decrement a number with JS  - Using -- to subtract 1 number
+let myVar = 70;
+myVar--; //69
+
+
+//Create Decimal numbers with JS
+let myDecimal = 3.8;
+
+
+//Multiply two decimals with JS
+let prod = 2.0 * 3.5; //7
+
+
+//Divide one Decimal by Another with JS
