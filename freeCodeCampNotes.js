4175480ee2caa2853825d3dd1dfd9499206dc6a7
Derek Danielson <derekdanielson84@gmail.com>
2020-11-16 12:41:03 -0700

    More notes

--- freeCodeCampNotes.js
+++ freeCodeCampNotes.js
@@ -139,5 +139,126 @@ newLine += goDo; //'I will get this!!!!'
 
 
 //Find the Length of a String
+let newName = 'pumpkinhead';
+newName.length; //10
+//******Another example
+let cool = 0;
+let newCool = 'ScoobyDoo';
+cool = newCool.length; // cool = 9
+
+
+//Use Bracket Notation to Find the First Character in a String
+let firstName = 'Dobobo';
+firstName[2]; //b
+//*****Another example
+let secondName = 'Charles';
+let firstLetter = secondName[1]; //h
+
+
+//Understand String Immutability
+//*******Cannot change characters in strings
+let myStr = 'Bob';
+myStr[0] = 'J'; //Won't work
+
+let myStr = 'Bob';
+myStr = 'Job'; //Works to change to 'Job'
+
+
+//Use Bracket Notation to Find the Nth Character in a String
+let firstFirst = 'Jedi';
+let secondSec = firstFirst[1]; //'e'
+
+
+//Use Bracket Notation to Find the Last Character in a String
+let firstCar = 'Dakota';
+let lastLetter = firstCar[firstCar.length - 1]; //'a'
+
+
+//Use Bracket Notation to Find the Nth-to-last Character in a String
+let firstCar = 'Dakota';
+let nthLast = firstCar[firstCar.length - 3]; //'o'
+
+
+//Mad libs
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+var wordBlanks = " " + myNoun + " " + myAdjective + " " + myVerb + " " + myAdverb;
+
+
+//Store Multiple Values in one Variable using Javascript Arrays
+let arrayo = ['Lucky', 13, 'Burrito']; //['Lucky', 13, 'Burrito']
+
+
+//Nest one Array within Another Array
+let arraYo = [['Lucky' 13], [2, 'Burrito' ]]; //[['Lucky' 13], [2, 'Burrito' ]]
+
+
+//Access Array Data with Indexes
+let argh = ['green', 40, 30];
+argh[2]; //30
+//or
+let pugh = argh[0]; //'green'
+
+
+//Modify Array Data with Indexes
+let argh = ['green', 38, 10];
+argh[0] = 13; //[13, 38, 10]
+
+
+//Access Multi-Dimensional Arrays with Indexes
+var arr = [
+    [3, 5, 2],
+    [6, 1, 8],
+    [[10, 9, 3], 5, 13]
+];
+
+arr[2]; //[[10, 9, 3], 5, 13]
+arr[2][1]; //[10, 9, 3]
+arr[2][1][0] //10
+
+
+//Manipulate Arrays with .push()  -------- Adds to end of Array
+var myArray = [["John", 23], ["cat", 2]];
+myArray.push(['dog', 3]); //[['John', 23], ['cat', 2], ['dog', 3]]
+
+
+//Manipulate Arrays with .pop()  -------- Removes end of Array
+var threeArr = [8, 3, 9];
+var oneDown = threeArr.pop();
+console.log(oneDown); //9
+console.log(threeArr); //[8, 3]
+
+
+//Manipulate Arrays with .shift()  -------- Removes first element of Array
+let newArr = ['Simpson', 5, ['Homer', 1]];
+let secAarr = newArr.shift();
+console.log(secArr); //'Simpson'
+console.log(newArr); //[5, ['Homer', 1]]
+
+
+//Manipulate Arrays with .unshift()  ------- Adds elements to front of Array
+let ourArray = ['Simpson', 5, ['Homer', 1]];
+ourArray.unshift('Bart'); //['Bart', 'Simpson', 5, ['Homer', 1]]
+
+//Example removing then adding first element
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.shift(); // ourArray now equals ["J", "cat"]
+ourArray.unshift("Happy"); // ourArray now equals ["Happy", "J", "cat"]
+
+
+
+//Example Function and the Function being called
+function reusableFunction() {
+    console.log('Hi World');
+}
+
+reusableFunction(); //'Hi world'
+
+
+//Passing Values to Functions with Arguments
+
 
 
